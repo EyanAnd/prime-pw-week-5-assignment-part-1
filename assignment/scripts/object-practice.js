f312f500b803bea1d0eb43ea2779d9e0e813e6a5
Eyan Anderson <eyananderson@gmail.com>
2023-04-17 10:56:13 -0500

    initialized  me object

--- assignment/scripts/object-practice.js
+++ assignment/scripts/object-practice.js
@@ -9,6 +9,11 @@ console.log('***** Object Practice *****')
   - Also give it an array 'favThreeFoods'
 */
 const me = {
+  firstName: 'Eyan', 
+  lastName: 'Anderson', 
+  hasSiblings: true, 
+  shoeCount: 13, 
+  favThreeFoods: ['peanut butter', 'rice', 'chicken']
   // TODO - add properties here
 };
 console.log('A little about me:', me);
